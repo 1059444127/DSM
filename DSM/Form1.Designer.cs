f1399d783fb6156f5f744f04e931451eab543d7f
luruhao <1062100362@qq.com>
2016-10-13 15:27:44 +0800

    DDD

---
+++ DSM/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace DSMUI
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// 必需的设计器变量。
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// 清理所有正在使用的资源。
+    /// </summary>
+    /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows 窗体设计器生成的代码
+
+    /// <summary>
+    /// 设计器支持所需的方法 - 不要
+    /// 使用代码编辑器修改此方法的内容。
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+      this.SuspendLayout();
+      // 
+      // labelControl1
+      // 
+      this.labelControl1.Location = new System.Drawing.Point(81, 41);
+      this.labelControl1.Name = "labelControl1";
+      this.labelControl1.Size = new System.Drawing.Size(70, 14);
+      this.labelControl1.TabIndex = 0;
+      this.labelControl1.Text = "labelControl1";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(644, 403);
+      this.Controls.Add(this.labelControl1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private DevExpress.XtraEditors.LabelControl labelControl1;
+  }
+}
+
---
+++ DSMBLL/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSMBLL
+{
+  public class Class1
+  {
+  }
+}
